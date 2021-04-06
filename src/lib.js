eda0dcbf7d450359f7e2fe3630ebcbc8a0e790e5
achingbrain <alex@achingbrain.net>
2021-04-06 08:00:47 +0100

    fix: browser field should point to cjs
    > a top-level "browser" field should point to CJS files that, when bundled by a non-broken module bundler, work in a browser
    
    https://github.com/browserify/browser-resolve/pull/101#issuecomment-806391082
    
    I've changed the `browser` field to point to the `cjs` version of this module in
    line with the above comment because this module is currently broken when used with
    browserify.
    
    I've also added the [util](https://www.npmjs.com/package/util) module as a dep since it's used
    in the code. It would be ignored in most environments so the only cost is a slightly larger
    bundle but it's likely to be included in any non-trivial bundle somewhere anyway so there's
    likely to be no real-world impact.
    
    Also:
    
    - Adds typescript dep to tests that test typescript, otherwise tests fail with
      'cannot determine executable to run' - unless you have tsc installed globally I guess?
    - Fixes a typo in a comment
    - Simplifies .gitignore
    
    Fixes #19

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 // In node `export { TextEncoder }` throws:
-// "Export 'TextEncoder' is not definedin module"
+// "Export 'TextEncoder' is not defined in module"
 // To workaround we first define constants and then export with as.
 const Encoder = TextEncoder
 const Decoder = TextDecoder
